dd3198698720633f7aa5cd1730a3ebab8b787925
Alberto156 <lm5708144@gmail.com>
2020-12-11 09:39:40 -0400

    base_proyect

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="Inicio">
+      <div class="form-container" v-if="form_pass == true">
+          <input type="text" name="channel_name" id="channels_name" placeholder="channel_name"  v-model="channel_name">
+          <input type="text" name="rol_user" id="rol_user" placeholder="rol_user" v-model="rol_user">
+          <button @click="onForm">Iniciar</button>
+      </div>
+
+      <hello-world v-else :channelName="channel_name" :rolUser="rol_user"> </hello-world>
+
+  </div>
+</template>
+
+<script>
+import AgoraRTC from "agora-rtc-sdk";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      channel_name : "",
+      rol_user : 1,
+      form_pass : true
+    };
+  },
+  components: {
+    HelloWorld,
+  },methods:{
+    onForm(){
+      this.form_pass = false
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="mainC">
+    <h1 class="titlemain">Probando Video llamadas</h1>
+    <p class="titlemain">activos : por definir</p>
+    <button v-on:click="join_client">Join</button>
+
+    <div id="app">
+      <div class="remote-c">
+        <p class="titlemain">Remote video</p>
+        <div id="remote-container"></div>
+        <div class="btn-controls">
+          <button>audio</button>
+          <button>video</button>
+          <button>exit</button>
+        </div>
+      </div>
+
+      <div class="container-user">
+        <p class="titlemain">Local video</p>
+        <div id="me"></div>
+        <div class="btn-controls">
+          <button>audio</button>
+          <button>video</button>
+          <button>exit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AgoraRTC from "agora-rtc-sdk";
+
+// Handle errors.
+let handleError = function (err) {
+  console.log("Esto no funciona: ");
+};
+
+// Remove the video stream from the container.
+function removeVideoStream(elementId) {
+  let remoteDiv = document.getElementById(elementId);
+  if (remoteDiv) remoteDiv.parentNode.removeChild(remoteDiv);
+}
+
+export default {
+  name: "HelloWorld",
+  data: function () {
+    return {
+      client: 0,
+    };
+  },
+
+  props: {
+    channelName: String,
+    rolUser: Number,
+  },
+
+  created() {
+    this.client = AgoraRTC.createClient({
+      mode: "rtc",
+      codec: "vp8",
+    });
+    this.client.init("3d2452ae22b54bd7b037d89b006c2cb8");
+  },
+
+  methods: {
+    join_client() {
+      // Query the container to which the remote stream belong.
+      let remoteContainer = document.getElementById("remote-container");
+      let response = this.get_token();
+
+      let localStream = AgoraRTC.createStream({
+        audio: true,
+        video: true,
+      });
+
+      let token = "0063d2452ae22b54bd7b037d89b006c2cb8IADsdUnIgqWD/bHbcaI6XOu1NDpiSQC1ogJQl9/KVhgY+wx+f9gAAAAAEABC9EaCQMLUXwEAAQBAwtRf" 
+      this.client.join(
+        token,
+        this.channelName,
+        null,
+        (uid) => {
+          // Create a local stream
+        },
+        handleError
+      );
+
+      // Initialize the local stream
+      localStream.init(() => {
+        // Play the local stream
+        localStream.play("me");
+        // Publish the local stream
+        this.client.publish(localStream, handleError);
+      }, handleError);
+
+      this.client.on("stream-added", function (evt) {
+        this.client.subscribe(evt.stream, {video: true, audio: false});
+      });
+      // Play the remote stream when it is subsribed
+      this.client.on("stream-subscribed", function (evt) {
+        let stream = evt.stream;
+        let streamId = String(stream.getId());
+        this.addVideoStream(streamId);
+        stream.play(streamId);
+      });
+    },
+
+    /*Get token of authentications*/
+    async get_token() {
+      let url = "http://localhost:8000/streamToken/generate_token/";
+      let data = { channel_name: this.channelName, rol_user: this.rolUser };
+      let response = await fetch(url, {
+        method: "POST", // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .catch((error) => console.error("Error:", error))
+        .then((response) => response);
+      return response;
+    },
+
+    /*Get token of authentications*/
+    addVideoStream(elementId) {
+      console.log("-----------------llamada-----------------------");
+      // Creates a new div for every stream
+      let streamDiv = document.createElement("div");
+      // Assigns the elementId to the div.
+      streamDiv.id = elementId;
+      // Takes care of the lateral inversion
+      streamDiv.style.transform = "rotateY(180deg)";
+      // Adds the div to the container.
+      remoteContainer.appendChild(streamDiv);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#app {
+  display: flex;
+  flex-direction: row;
+  margin: 10px 5%;
+  width: 90%;
+  border: 1px solid #000;
+  height: 90vh;
+  position: relative;
+}
+
+.titlemain {
+  text-align: center;
+}
+
+#me {
+  width: 350px;
+  height: 200px;
+  background-color: aqua;
+}
+
+#remote-container {
+  width: 350px;
+  height: 200px;
+  margin: 5px;
+  background-color: aqua;
+}
+
+.remote-c {
+  position: relative;
+}
+
+.btn-controls {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+</style>
